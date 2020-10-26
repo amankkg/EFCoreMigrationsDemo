06e73829fc05eadd606e9592fdd052f4354f1611
amankkg <amankubanychbek@gmail.com>
2020-10-26 20:42:15 +0600

    Add project files.

---
+++ EFCoreMigrations/Migrations/20201026140945_Initial.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace EFCoreMigrations.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Residents",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Title = table.Column<string>(nullable: true),
+                    Activity = table.Column<string>(nullable: true),
+                    Entered = table.Column<DateTime>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Residents", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Residents");
+        }
+    }
+}
---
+++ EFCoreMigrations/Program.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace EFCoreMigrations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+
+    class HTPContext : DbContext
+    {
+        public DbSet<Resident> Residents { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder builder)
+        {
+            builder.UseSqlServer(@"Server=AMANK-XPS-9360\SQLEXPRESS; Database=EFCoreMigrations; Integrated Security=true");
+        }
+    }
+
+    class Resident
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Activity { get; set; }
+        public DateTime? Entered { get; set; }
+    }
+}
